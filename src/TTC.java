bbee6d8f021335f64548c65814bd5cdf32bcf80c
manngill <manpreetgill1946@gmail.com>
2019-05-27 12:35:49 -0400

    R1--GREEN

--- src/TTC.java
+++ src/TTC.java
@@ -3,6 +3,14 @@ public class TTC {
 	
 
 	public double calculateTotal(String[] from, String[] to) {
+	String[] from1 = new String[]{"Leslie"};
+
+	String[] to1 = new String[]{"Don Mill"};
+	from[0].contains(("Leslie") && to[0].contains("Don Mills"));{
+		return 2.5;
+	}
+	
+	
 		
 		return 0.0;
 	}
