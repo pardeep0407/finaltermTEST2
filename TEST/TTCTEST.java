a6c0e3d72589a75f79d6d7fd220e21a3e12fb082
manngill <manpreetgill1946@gmail.com>
2019-05-27 12:50:01 -0400

    r3--red

--- TEST/TTCTEST.java
+++ TEST/TTCTEST.java
@@ -22,13 +22,23 @@ public class TTCTEST {
 		
 	
 	}
-	//public void test2() {
-		//TTC T = new TTC();
+	@Test
+	public void test2() {
+		TTC T = new TTC();
 		//inside zone2
-		//String[] from = new String []{"Leslie"};
-		//String[] To = new String []{"Don Mills"};
-		//double fare = T.calculateTotal(from,To);
-		//assertEquals(2.50,fare,0.0);
-	//}	
+		String[] from1 = new String []{"Sheppard"};
+		String[] To1 = new String []{"Finch Station"};
+		double fare = T.calculateTotal(from1,To1);
+		assertEquals(3.0,fare,0.0);
+	}	
+	@Test
+      public void test3() {
+		TTC T = new TTC();
+		//trip between zones
+		String[] from1 = new String []{"Don Mills"};
+		String[] To1 = new String []{"Finch Station"};
+		double fare = T.calculateTotal(from1,To1);
+		assertEquals(3.0,fare,0.0);
+	}	
 
 }
